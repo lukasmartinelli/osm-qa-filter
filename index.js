072c0930bccbb176d834d7952f91273f42131880
Lukas Martinelli <me@lukasmartinelli.ch>
2016-12-28 14:45:20 +0100

    Add map script

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const tileReduce = require('tile-reduce');
+const path = require('path');
+const fs = require('fs');
+const geoJSONStream = require('geojson-stream');
+const program = require('commander');
+
+program
+    .option('-o, --geojson-file <f>', 'GeoJSON target file')
+    .option('-f, --filter <ff>', 'Feature filter')
+    .option('-m, --mbtiles-file <f>', 'MBTiles source file')
+    .parse(process.argv);
+
+if(program.mbtilesFile && program.geojsonFile && program.filter) {
+    const outputStream = fs.createWriteStream(program.geojsonFile);
+    const featureStream = geoJSONStream.stringify();
+    featureStream.pipe(outputStream);
+
+    let foundFeatureCount = 0;
+    tileReduce({
+      zoom: 12,
+      map: path.join(__dirname, '/map.js'),
+      mapOptions: {
+        filter: JSON.parse(program.filter),
+      },
+      sources: [{
+        name: 'osm',
+        mbtiles: path.normalize(program.mbtilesFile),
+        raw: false
+      }]
+    })
+    .on('reduce', (features, tile) => {
+      foundFeatureCount += features.length;
+      features.forEach(function(ft) {
+        featureStream.write(ft);
+      });
+    })
+    .on('end', () => {
+      featureStream.end();
+      console.log('Total found features: %d', foundFeatureCount);
+    });
+} else {
+    program.help();
+}
---
+++ map.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const featureFilter = require('mapbox-gl-style-spec/lib/feature_filter');
+const turf = require('turf');
+
+const ff = featureFilter(global.mapOptions.filter)
+
+module.exports = function(tileLayers, tile, write, done) {
+  const layer = tileLayers.osm.osm;
+  const features = layer.features.filter(function(ft) {
+    return ff(ft)
+  })
+  done(null, features);
+};
